bfabb532a15026fb005c496e8dce217ae514c288
Todd Fleeman <rtoddf@comcast.net>
2019-06-23 13:20:27 -0400

    almost done with the lesson

--- FBGroups01/FBGroups01/ContentView.swift
+++ FBGroups01/FBGroups01/ContentView.swift
@@ -1,4 +1,4 @@
-//got to 18:34
+//got to 29.48
 
 import SwiftUI
 
@@ -9,28 +9,29 @@ struct Post {
 
 struct ContentView : View {
     let posts: [Post] = [
-        .init(id: 0, thumbnail: "franta", username: "Connor Franta", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus eros sit amet lorem finibus, et varius ante interdum.", imageName: "franta_large"),
-        .init(id: 1, thumbnail: "crawford", username: "Matthew Crawford", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus eros sit amet lorem finibus, et varius ante interdum.", imageName: "crawford_large"),
-        .init(id: 2, thumbnail: "dylan", username: "Dylan Geick", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus eros sit amet lorem finibus, et varius ante interdum.", imageName: "dylan_large")
+        .init(id: 0, thumbnail: "dylan", username: "Dylan Geick", text: "Sed elementum eleifend arcu ut commodo. Cras sit amet pulvinar odio, at consequat nisi. Proin magna sem, dapibus eu tellus a, placerat efficitur arcu.", imageName: "dylan_large"),
+        .init(id: 1, thumbnail: "crawford", username: "Matthew Crawford", text: "Praesent eu venenatis dui, scelerisque posuere erat. Aliquam et lacus dignissim, interdum velit non, ultricies ante. Praesent at venenatis quam. Phasellus porta ex lorem, sit amet sagittis dolor vehicula vitae.", imageName: "crawford_large"),
+        .init(id: 2, thumbnail: "franta", username: "Connor Franta", text: "Etiam ultrices, quam eu sodales pretium, ex diam aliquet turpis, ut tincidunt erat justo sed dui.", imageName: "franta_large"),
+        .init(id: 3, thumbnail: "yang", username: "Eugene Lee Yang", text: "Duis feugiat metus id augue consectetur, a porta mauris ultricies. Duis id arcu at neque fermentum cursus. Pellentesque suscipit, mi sit amet ullamcorper tempor, quam massa consectetur nisi, id consectetur odio nulla fermentum est. Sed in fermentum orci.", imageName: "yang_large"),
+        .init(id: 4, thumbnail: "yummertime", username: "Yummertime", text: "Pellentesque finibus eleifend justo, eu consequat nulla rhoncus vitae. Fusce at scelerisque lectus, a molestie ligula.", imageName: "yummertime_large"),
+        .init(id: 5, thumbnail: "pj-thomas", username: "PJ & Thomas", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus eros sit amet lorem finibus, et varius ante interdum.", imageName: "pj-thomas_large")
     ]
     
     var body: some View {
         NavigationView {
             List {
-                
-                ScrollView {
-                    VStack (alignment: .leading) {
-                        Text("Trending")
-                        HStack {
-                            Text("number one")
-                            Text("number one")
-                            Text("number one")
-                            Text("number one")
-                            Text("number one")
-                            Text("number one")
+                VStack (alignment: .leading) {
+                    Text("Trending")
+                    ScrollView {
+                        VStack (alignment: .leading) {
+                            HStack {
+                                ForEach(posts.identified(by: \.id)){ post in
+                                    TrendingPostView(post: post)
+                                }
+                            }
                         }
-                    }
-                }.frame(height: 100)
+                    }.frame(height: 190)
+                }.padding(.bottom, -8)
                 
                 // post rows
                 ForEach(posts.identified(by: \.id)){ post in
@@ -38,6 +39,22 @@ struct ContentView : View {
                 }
             }.navigationBarTitle(Text("Youtubers"))
         }
+    }
+}
+
+struct TrendingPostView: View {
+    let post: Post
+    
+    var body: some View {
+        VStack (alignment: .leading) {
+            Image(post.thumbnail)
+                .resizable()
+                .frame(width: 120, height: 120)
+                .clipped()
+                .padding(.leading, 0)
+            Text(post.username).lineLimit(nil)
+            Spacer()
+            }.frame(width: 120, height: 180)
     }
 }
 
@@ -64,10 +81,10 @@ struct PostView: View {
             Text(post.text)
                 .lineLimit(nil)
                 .padding(.leading, 16)
-                .padding(.trailing, 32)
+                .padding(.trailing, 8)
             Image(post.imageName)
                 .resizable()
-                .aspectRatio(CGSize(width: 1.500, height: 1.0), contentMode: .fit)
+                .aspectRatio(CGSize(width: 16, height: 9), contentMode: .fit)
                 .padding(.trailing, -32)
             
         }.padding(.leading, -20).padding(.bottom, -8)
